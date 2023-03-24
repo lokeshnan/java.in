62bb6da1fb20b108a94281cbaff2280702e48525
lokeshnan <128789128+lokeshnan@users.noreply.github.com>
2023-03-24 15:07:47 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Division{
+       	public static void main(String[] args) {
+            		System.out.println("No are: 50 5");
+             		int a=50,b=5;
+            		int res=a/b;
+	            	System.out.print("Division is "+res);
+	                  		}
+}
