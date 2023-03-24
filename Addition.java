826d979925dae35e6cc3f5b08fd5bf483fc58111
lokeshnan <128789128+lokeshnan@users.noreply.github.com>
2023-03-24 15:04:54 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Addition{
+      	public static void main(String[] args) {
+	             	int a=40,b=80;
+             	 	int res=a+b;
+	             	System.out.print("Addition is "+res);
+	          		}
+}
