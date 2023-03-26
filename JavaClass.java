96865762ef6ec6634bc4723677d95039e4836995
lokeshnan <128789128+lokeshnan@users.noreply.github.com>
2023-03-26 22:53:39 +0530

    Add files via upload

---
+++ JavaClass.java
@@ -0,0 +1,23 @@
+package javaPackage;
+
+public class JavaClass {
+	
+	int d =666;
+	static int a=10;
+	static int b= 20;
+	String p = " Soham ";
+	public static void demo(){
+	 int c = a+b;
+		System.out.println(a);
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO JAVA");
+		demo();
+		
+		//System.out.println(p);
+	}
+
+}
