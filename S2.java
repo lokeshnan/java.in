c9f1bd1da4b97bcfcde19c6090144d32b97e56c1
lokeshnan <128789128+lokeshnan@users.noreply.github.com>
2023-03-27 00:30:12 +0530

    Add files via upload

---
+++ S2.java
@@ -0,0 +1,19 @@
+package javaPackage;
+
+import java.util.Scanner;
+
+public class S2 {
+   public static void main(String[] args) 
+   {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter your age: ");
+      int age = scanner.nextInt();
+      
+      if (age >= 18)
+         System.out.println("You are eligible to vote.");
+       else
+         System.out.println("You are not eligible to vote.");
+      
+      
+   }
+}
